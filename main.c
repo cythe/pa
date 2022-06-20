8ad4b82fa2d8232dc0a0bfe3179e250cb440b7f9
Ruiqiang Hao <Ruiqiang.Hao@windriver.com>
2022-06-20 15:08:15 +0800

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define A 10
+#define B(X) (X+12)
+
+int a;
+
+void foo(void *ppt)
+{
+    print("foo\n");
+}
+
+int main(int argc, char *argv[])
+{
+    foo(NULL);
+
+    return 0;
+}
