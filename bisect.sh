32fbf10ba45f5b2df8586acf1dc43b81cdd0bfcc
Ruiqiang Hao <Ruiqiang.Hao@windriver.com>
2024-07-02 03:47:17 +0000

    bisect.sh: use function instead of progress
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- bisect.sh
+++ bisect.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
 PDIR=$(dirname $(readlink -f $0))
-. $PDIR/color
+. $PDIR/utils
 
 WORK_DIR=`pwd`
-
-if [ -f bisect.conf ]; then
-    echo_c SC_GREEN "Read ppre from config."
-    PPRE=`cat bisect.conf`
-else
-    echo_c SC_GREEN "Try to find a ppre..."
-    PPRE=`find tmp-glibc/work -maxdepth 4 -name "recipe-sysroot-native" | grep linux-yocto`
-    if [ "x$PPRE" == "x" ] ; then
-	echo_c SC_RED "I can't found recipe-sysroot-native dir..."
-	exit 1
-    fi
-    echo $PPRE > bisect.conf
-fi
-
-export PATH=$PATH:$WORK_DIR/$PPRE/usr/bin/aarch64-wrs-linux
 
 ARCH=arm64
 CROSS=aarch64-wrs-linux-
@@ -27,26 +12,29 @@ OUT_DIR=$WORK_DIR/obj_out
 THREADs=-j96
 CONFIG_F=config
 
-if [ ! -x $KERNEL_DIR/patch ]; then
-    ln -sf ~/scripts/patch $KERNEL_DIR/patch
-fi
-if [ ! -x $KERNEL_DIR/rejclean ]; then
-    ln -sf ~/scripts/rejclean $KERNEL_DIR/rejclean
-fi
-if [ ! -x $KERNEL_DIR/update ]; then
-    ln -sf ~/scripts/update $KERNEL_DIR/update
-fi
-
-if [ "$1" = "clean" ]; then
-    make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR clean
-    if [ "$?" != "0" ]; then
-	echo_c SC_RED "failed."
-	exit 1
+set_env() {
+    if [ -f bisect.conf ]; then
+	echo_c SC_GREEN "Read ppre from config."
+	PPRE=`cat bisect.conf`
+    else
+	echo_c SC_GREEN "Try to find a ppre..."
+	PPRE=`find tmp-glibc/work -maxdepth 4 -name "recipe-sysroot-native" | grep linux-yocto`
+	if [ "x$PPRE" == "x" ] ; then
+	    echo_c SC_RED "I can't found recipe-sysroot-native dir..."
+	    exit 1
+	fi
+	echo $PPRE > bisect.conf
     fi
-    exit 0
-fi
+    export PATH=$PATH:$WORK_DIR/$PPRE/usr/bin/aarch64-wrs-linux
+}
 
-if [ "$1" = "allyesmake" ]; then
+pushd $KERNEL_DIR
+if [ ! -x patch ]; then
+    $PDIR/install-flow-for-this-kernel-tree.sh
+fi
+popd
+
+allyesmake() {
     rm -rf $OUT_DIR
     make -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR allyesconfig
     make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR > /dev/null
@@ -55,9 +43,9 @@ if [ "$1" = "allyesmake" ]; then
 	exit 1
     fi
     exit 0
-fi
+}
 
-if [ "$1" = "menuconfig" ]; then
+menuconfig() {
     make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR menuconfig
     if [ "$?" != "0" ]; then
 	echo_c SC_RED "failed."
@@ -66,9 +54,9 @@ if [ "$1" = "menuconfig" ]; then
 	cp $OUT_DIR/.config $CONFIG_F 
     fi
     exit 0
-fi
+}
 
-if [ "$1" = "make" ]; then
+make() {
     cp $CONFIG_F $OUT_DIR/.config
     make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR
     make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR modules_install INSTALL_MOD_PATH=$OUT_DIR/__mod/
@@ -77,37 +65,59 @@ if [ "$1" = "make" ]; then
 	exit 1
     fi
     exit 0
-fi
+}
 
-patches_tmp=""
-patches_failed=0
-while true
-do
+bisect(){
 
-pushd $KERNEL_DIR
-./patch 1
-if [ "$?" != "0" ]; then
-    break
-fi
-popd
-
-cp $CONFIG_F $OUT_DIR/.config
-make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR
-if [ "$?" != "0" ]; then
-    echo_c SC_RED "failed."
-    patches_tmp="$patches_tmp $curr"
-    patches_failed=$(($patches_failed + 1))
-else
-    cp $OUT_DIR/.config $CONFIG_F 
     patches_tmp=""
     patches_failed=0
-fi
+    while true
+    do
+	pushd $KERNEL_DIR
+	./patch 1
+	if [ "$?" != "0" ]; then
+	    break
+	fi
+	popd
 
-if [ $patches_failed -gt 1 ]; then
-    echo_c SC_RED "2 patches failed. exit"
-    echo "$patches_tmp"
-    exit 1
-fi
+	cp $CONFIG_F $OUT_DIR/.config
+	make $THREADs -C $KERNEL_DIR ARCH=$ARCH CROSS_COMPILE=$CROSS O=$OUT_DIR
+	if [ "$?" != "0" ]; then
+	    echo_c SC_RED "failed."
+	    patches_tmp="$patches_tmp $curr"
+	    patches_failed=$(($patches_failed + 1))
+	else
+	    cp $OUT_DIR/.config $CONFIG_F 
+	    patches_tmp=""
+	    patches_failed=0
+	fi
 
-done
+	if [ $patches_failed -gt 1 ]; then
+	    echo_c SC_RED "2 patches failed. exit"
+	    echo "$patches_tmp"
+	    exit 1
+	fi
+    done
+}
 
+set_env
+
+case "$1" in
+    "make")
+	make
+	;;
+    "menuconfig")
+	menuconfig
+	;;
+    "allyesmake")
+	allyesmake
+	;;
+    *)
+	echo_c SC_B_CYAN "Usage1: $0"
+	echo_c SC_B_CYAN "\tbisect Verify"
+	echo
+	echo_c SC_B_CYAN "Usage2: $0 [cmd]"
+	echo_c SC_B_CYAN "cmd:"
+	echo_c SC_B_CYAN "\tmake menuconfig allyesmake"
+	exit -1
+esac
